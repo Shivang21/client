a8e026ac8b219fd99603e962026d25cd572e34ba
Shivang21 <33567762+Shivang21@users.noreply.github.com>
2024-07-09 21:49:48 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-const sockets = io('https://servervideo-jvz7.onrender.com');
+const sockets = io('https://server-service-4g2g.onrender.com');
 export default sockets;
